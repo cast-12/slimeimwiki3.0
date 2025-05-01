36b18fe488345fd56dcf975f92d89d767ebd96e2
Cast <55102343+cast-12@users.noreply.github.com>
2025-04-30 22:18:19 -0400

    updates

--- pages/tierlist/index.js
+++ pages/tierlist/index.js
@@ -32,7 +32,14 @@ export default function Home({ storage }) {
 		filter: filter,
 		setFilter: setFilter,
 		AlternateCharacterIcon: AlternateCharacterIcon,
+		confirmFunction: (id) => {
+			const date = characters[id]?.Release;
+			if (!date) return true;
+			const year = parseInt(date.split("/")[2]);
+			return year > 2022;
+		}
 	};
+	
 
 	return (
 		<>
